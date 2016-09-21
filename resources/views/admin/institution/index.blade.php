4530b8e6c2c67dc426239e88a3a9da38656c2dbb
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2016-09-21 01:24:25 +0000

    button Agregar removed, use <a> instead of <button>

--- resources/views/admin/institution/index.blade.php
+++ resources/views/admin/institution/index.blade.php
@@ -8,15 +8,15 @@
         <div class="panel panel-info">
             <div class="panel-heading text-center">
                 <h3  style="display: inline-block;">Instituciones</h3>
-                <button type="button" class="btn btn-success btn-header" title="Agregar">
+                <a type="button" class="btn btn-success btn-header" title="Agregar" href="{{ route('institutions.create') }}">
                   <span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
-                </button>
-                <button type="button" class="btn btn-info btn-header" v-show="institution_selected" title="Editar">
+                </a>
+                <a type="button" class="btn btn-info btn-header" v-show="institution_selected" title="Editar">
                   <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-                </button>
-                <button type="button" class="btn btn-danger btn-header" v-show="institution_selected" title="Eliminar">
+                </a>
+                <a type="button" class="btn btn-danger btn-header" v-show="institution_selected" title="Eliminar">
                   <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
-                </button>
+                </a>
             </div>
             <div class="panel-body">
                 <div class="col-sm-12 col-md-10 col-md-offset-1">
@@ -37,9 +37,6 @@
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row">
-        <a href="{{ route('institutions.create') }}" class="btn btn-success">Agregar</a>
     </div>
 @endsection
 
