879661fb8d21877c1a571cc178ace13465f65bbe
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2016-10-08 13:58:38 +0000

    buscar y traer todas las instituciones

--- app/Models/Institucion.php
+++ app/Models/Institucion.php
@@ -24,4 +24,10 @@ class Institucion extends Model
     {
         return $this->hasMany(Facultad::class, 'institucion_id');
     }
+
+    public function scopeSearch($query, $nombre)
+    {
+        return $query->where('nombre', 'LIKE', '%'. $nombre . '%')
+                     ->orWhere('siglas', 'LIKE', '%'. $nombre . '%');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::group(['prefix' => 'instituciones', 'as' => 'instituciones.'], function() {
+    Route::get('/', ['as' => 'index', 'uses' => 'InstitucionController@index']);
+    Route::get('/buscar', ['as' => 'index', 'uses' => 'InstitucionController@search']);
+});
