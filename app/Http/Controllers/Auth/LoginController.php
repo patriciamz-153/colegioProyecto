6a6eed4d680dffef8d06fee020878ecb479d2581
Junior Claudio <junior.claudio@tektonlabs.com>
2016-10-13 16:17:19 +0000

    firewall dependecy added

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,7 +36,7 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest', ['except' => 'logout']);
+        $this->middleware('fw-block-bl', ['except' => 'logout']);
     }
 
     /**
