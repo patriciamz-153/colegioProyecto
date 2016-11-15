be5786b9669eebb14180a161622841513fbbfca5
Pedro Manuel Díaz Santa María <pedro@diazsantamaria.com>
2016-11-15 03:50:45 -0500

    sql contact

---
+++ database/contactos.sql
@@ -0,0 +1,23 @@
+﻿# Host: 192.168.10.10  (Version 5.7.12)
+# Date: 2016-11-15 03:49:50
+# Generator: MySQL-Front 5.4  (Build 1.6)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "contactos"
+#
+
+DROP TABLE IF EXISTS `contactos`;
+CREATE TABLE `contactos` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+#
+# Data for table "contactos"
+#
+
